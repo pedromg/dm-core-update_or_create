e30a0b5858af739597bc1486c878f741f469a020
pedro mg <pedro@tquadrado.com>
2011-07-18 10:28:34 -0700

    Edited documentation

--- update_or_create.rb
+++ update_or_create.rb
@@ -4,14 +4,11 @@ module DataMapper
     # update_or_create method: finds and updates, or creates;
     #   -upon create, returns the object
     #   -upon update, returns the object (by default, returned True)
-    # 
-    # Params:
-    #   -merger is a boolean that determines if the conditions are merged with the attributes upon create;
-    #   -merge = true => merges conditions to attributes and passes the merge to the create method
-    #   -merge = false => only attributes are passed into the create method
-    #
-    # Older implementation:
-    #   (first(conditions) && first(conditions).update(attributes)) || create(merger ? (conditions.merge(attributes)) : attributes )
+    # @param[Hash] Conditions hash for the search query.
+    # @param[Hash] Attributes hash with the property value for the update or creation of a new row.
+    # @param[Boolean] Merger is a boolean that determines if the conditions are merged with the attributes upon create.
+    #   If true, merges conditions to attributes and passes the merge to the create method;
+    #   If false, only attributes are passed into the create method
     def update_or_create(conditions = {}, attributes = {}, merger = true)
       begin
 	if (row = first(conditions))
